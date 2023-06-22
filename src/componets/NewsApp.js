31693f441be31c1039ae90a2bc909596c111ee62
jaswanthkumarpolisetty <jaswanthkumarpolisetty@gmail.com>
2023-06-22 09:15:57 +0530

    finally done

--- src/componets/NewsApp.js
+++ src/componets/NewsApp.js
@@ -3,11 +3,11 @@ import News from './News';
 import './NewsApp.css';
 
 function NewsApp() {
-  const apiKey = '77c8d9571ded469485630a4f24a27456';
-
+  const apiKey = '77c8d9571ded469485630a4f24a27456'; // Retrieve API key from environment variable
   const [newsList, setNewsList] = useState([]);
   const [query, setQuery] = useState('cricket');
   const queryInputRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -15,15 +15,23 @@ function NewsApp() {
 
   async function fetchData() {
     try {
-      const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-05-21&sortBy=publishedAt&apiKey=${apiKey}`;
+      setIsLoading(true);
+
+      const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-06-21&sortBy=publishedAt&apiKey=${apiKey}`;
       const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch data from the API.');
+      }
+
       const jsonData = await response.json();
       setNewsList(jsonData.articles);
+      setIsLoading(false);
     } catch (error) {
-      console.log(error, 'error occurred');
+      setIsLoading(false);
+      console.log(error, 'An error occurred while fetching data');
     }
   }
-  
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -33,7 +41,9 @@ function NewsApp() {
 
   return (
     <div className='container'>
-      <h1 style={{fontFamily:'monospace', fontSize:'3rem' ,textAlign: 'left',marginBottom:'20px'}}>News Daily</h1>
+      <h1 style={{ fontFamily: 'monospace', fontSize: '3rem', textAlign: 'left', marginBottom: '20px' }}>
+        News Daily
+      </h1>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -41,7 +51,7 @@ function NewsApp() {
           className="search-input"
           placeholder="Enter your search term..."
         />
-        <br/>
+        <br />
         <button type="submit" className="search-button">
           Search
         </button>
@@ -54,15 +64,18 @@ function NewsApp() {
           rowGap: '20px'
         }}
       >
-        {newsList.map(news => (
-          <News key={news.url} news={news} />
-        ))}
+        {newsList.length === 0 && !isLoading ? (
+          <p>No results found.</p>
+        ) : (
+          newsList.map(news => <News key={news.title} news={news} />)
+        )}
       </div>
     </div>
   );
 }
 
 export default NewsApp;
+
 
 
 
